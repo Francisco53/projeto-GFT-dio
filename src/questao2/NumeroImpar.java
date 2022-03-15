9428b8e1d028db027766df3cec26337290631ede
Francisco53 <franciscolima2228648@gmail.com>
2022-03-15 18:09:32 -0300

    atualizacao do teste 2

--- src/questao2/NumeroImpar.java
+++ src/questao2/NumeroImpar.java
@@ -1,5 +1,6 @@
 package questao2;
 
+import javax.crypto.spec.PSource;
 import java.util.Scanner;
 
 public class NumeroImpar {
@@ -9,28 +10,33 @@ public class NumeroImpar {
         Scanner sc = new Scanner(System.in);
         char[] digits1;
 
-        do{
-            int numero;
+        int numero;
 
-            numero = sc.nextInt();
+        numero = sc.nextInt();
 
-            String number = String.valueOf(numero);
+        if(numero >= 350 && numero <= 8750){
+            do{
 
-            digits1 = number.toCharArray();
-            if(digits1.length < 4)
-                System.out.println("O número precisa conter pelo menos 3 dígitos");
+                String number = String.valueOf(numero);
 
-        }while (digits1.length < 3);
+                digits1 = number.toCharArray();
+                if(digits1.length < 4)
+                    System.out.println("O número precisa conter pelo menos 3 dígitos");
 
-        int contador = 0;
-        for(int i = 0; i < digits1.length; i++) {
-            if(digits1[i] % 2 != 0){
-                contador ++;
+            }while (digits1.length < 3);
+
+            int contador = 0;
+            for(int i = 0; i < digits1.length; i++) {
+                if(digits1[i] % 2 != 0){
+                    contador ++;
+                }
             }
+
+            System.out.println(contador);
         }
-
-        System.out.println(contador);
-
+        else
+            System.out.println("Número deve estar entre 350 e 8750");
 
     }
+
 }
